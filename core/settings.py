3728d53ce927232f2082059f6202c6a1f4d2b37b
Timothy <timothy.yeremia.wewengkang@gmail.com>
2026-04-05 11:13:31 +1000

    preparing for render deployment

--- core/settings.py
+++ core/settings.py
@@ -162,3 +162,8 @@ CACHES = {
         'LOCATION': 'unique-snowflake',
     }
 }
+
+# Add Render.com URL to allowed hosts
+ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+ if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
